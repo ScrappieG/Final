cd96b8c1724e62c65293cc6a7b7f137e50864d12
guauguauguau <tp.neubert@ufl.edu>
2024-04-27 11:13:58 -0400

    added comments

--- cell.py
+++ cell.py
@@ -6,8 +6,12 @@ class Cell:
         self.row = row
         self.col = col
         self.screen = screen
+
         self.sketched_value = None
         self.selected = False
+
+        # Tracks if this cell is one that contains a generated number, or is a player controlled cell.
+
         self.generated = False
 
         pass
@@ -27,10 +31,14 @@ class Cell:
         sketched_text = sketch_font.render(self.sketched_value, 0, (100, 100, 100))
         locked_text = number_font.render(self.value, 0, (0, 0, 0))
 
+        # IF the cell has sketched and real values, draws those values.
+
         if self.sketched_value != "":
             self.screen.blit(sketched_text, dest=[185 + self.col * 60, 185 + self.row * 60])
         if self.value != "":
             self.screen.blit(locked_text, dest=[198 + self.col * 60, 191 + self.row * 60])
+
+        # draws the selection box if the cell is selected.
 
         if self.selected:
             selection_box = pygame.draw.rect(self.screen, "orange", [180 + self.col * 60, 180 + self.row * 60, 60, 60], 3)
